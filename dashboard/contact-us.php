8affc37a4b35a62cb65cbfa5ad59dfab88a0bbaf
Joel Knechtli <8799535+jknechtli@user.noreply.github.com>
2021-05-13 15:37:00 +1000

    fixed contact us page

--- dashboard/contact-us.php
+++ dashboard/contact-us.php
@@ -1,76 +1,77 @@
 <html lang="en-AU">
-  <head>
-    <title>Contact Us</title>
-    <?php include '../shared/head.html';?>
-  </head>
-  <body>
-    <div class="container">
-      <?php include '../shared/header.html';?>
-      <div class="contents">
-        <div class="heading">
-          <h1>CONTACT US</h1>
-        </div>
-        <div class="description">
-          <p>
-            Thank you very much for your interest in Exceptional Study
-            Australia. If you would like to organise any resume writing with us
-            or would like any more information about what we do, please feel
-            free to contact us via any of following methods:
-          </p>
-          <ul>
-            <li>Phone / Text: 0447 840 939</li>
-            <li>Email: exceptionalstudyaustralia@gmail.com</li>
 
-            <li>
-              Office Hours: Monday – Friday 9am – 6pm / Saturday 9am - 3pm
-            </li>
-          </ul>
+<head>
+  <title>Contact Us</title>
+  <?php include '../shared/head.html'; ?>
+</head>
 
-          ​
-          <h3>Connect With Us on Social Media</h3>
-
-          ​
-          <p>
-            Facebook:
-            <a href="https://www.facebook.com/exceptionalstudyaustralia/">
-              https://www.facebook.com/exceptionalstudyaustralia/
-            </a>
-          </p>
-          <p>
-            Twitter:
-            <a href="https://twitter.com/ExceptionalAust">
-              https://twitter.com/ExceptionalAust
-            </a>
-          </p>
-          <p>
-            Instagram:
-            <a
-              href="https://www.instagram.com/exceptional_study_australia/?hl=en"
-            >
-              https://www.instagram.com/exceptional_study_australia/?hl=en
-            </a>
-          </p>
-          <p>
-            LinkedIn:
-            <a
-              href="https://au.linkedin.com/company/exceptional-study-australia"
-            >
-              https://au.linkedin.com/company/exceptional-study-australia
-            </a>
-          </p>
-          <p>
-            Youtube:
-            <a href="https://www.youtube.com/channel/UCa1502fgXTAEJ_ah4wa64Dw">
-              https://www.youtube.com/channel/UCa1502fgXTAEJ_ah4wa64Dw
-            </a>
-          </p>
-        </div>
-        <?php include '../shared/enquiries.html'; ?>
+<body>
+  <div class="container">
+    <?php include '../shared/header.html'; ?>
+    <div class="contents">
+      <div class="heading">
+        <h1>CONTACT US</h1>
       </div>
-      <div class="footer">
-<?php include '../shared/footer.html';?>
+      <div class="description">
+        <img src="/img/contact-us.webp" alt="">
+        <p>
+          Thank you very much for your interest in Exceptional Study
+          Australia. If you would like to organise any resume writing with us
+          or would like any more information about what we do, please feel
+          free to contact us via any of following methods:
+        </p>
+        <ul>
+          <li><span class="bold">Phone / Text:</span> 0447 840 939</li>
+          <li><span class="bold">Email:</span> exceptionalstudyaustralia@gmail.com</li>
+          <!-- <li>
+            Office Hours: Monday – Friday 9am – 6pm / Saturday 9am - 3pm
+          </li> -->
+        </ul>​
+        <h3>Connect With Us on Social Media</h3>​
+        <p>
+          <span class="bold"> Facebook: </span>
+          <a href="https://www.facebook.com/exceptionalstudyaustralia/">
+            facebook.com/exceptionalstudyaustralia/
+          </a>
+        </p>
+        <p>
+          <span class="bold"> Twitter: </span>
+          <a href="https://twitter.com/ExceptionalAust">
+            twitter.com/ExceptionalAust
+          </a>
+        </p>
+        <p>
+          <span class="bold">Instagram:</span>
+          <a href="https://www.instagram.com/exceptional_study_australia/?hl=en">
+            instagram.com/exceptional_study_australia/?hl=en
+          </a>
+        </p>
+        <p>
+          <span class="bold">LinkedIn:</span>
+          <a href="https://au.linkedin.com/company/exceptional-study-australia">
+            au.linkedin.com/company/exceptional-study-australia
+          </a>
+        </p>
+        <p>
+          <span class="bold">Youtube:</span>
+          <a href="https://www.youtube.com/channel/UCa1502fgXTAEJ_ah4wa64Dw">
+            youtube.com/channel/UCa1502fgXTAEJ_ah4wa64Dw
+          </a>
+        </p>
+        <p>
+          <span class="bold">TikTok:</span>
+          <a href="exceptionalstudyau">
+            exceptionalstudyau
+          </a>
+        </p>
       </div>
+      <?php include '../shared/enquiries.html'; ?>
     </div>
-  </body>
-  <?php include '../shared/script.html';?>
+    <div class="footer">
+      <?php include '../shared/footer.html'; ?>
+    </div>
+  </div>
+</body>
+<?php include '../shared/script.html'; ?>
+
 </html>
--- dashboard/index.php
+++ dashboard/index.php
@@ -283,20 +283,16 @@
 </body>
 <?php
 if (isset($_POST['submit'])) {
-  echo "ass";
-  $to = "jknechtli@hotmail.com"; // this is your Email address
+  $to = "exceptionalstudyaustralia@gmail.com"; // this is your Email address
   $from = $_POST['Email']; // this is the sender's Email address
   $first_name = $_POST['Name'];
   $subject = "Form submission";
-  $subject2 = "Copy of your form submission";
   $message = $first_name . " wrote the following: hi";
-  $message2 = "Here is a copy of your message " . $first_name . "\n\n" . "hi";
 
   $headers = "From:" . $from;
   // $headers2 = "From:" . $to;
   mail($to, $subject, $message, $headers);
-  mail($from, $subject2, $message2, $headers2); // sends a copy of the message to the sender
-  echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+  // echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
   // You can also use header('Location: thank_you.php'); to redirect to another page.
 }
 ?>
